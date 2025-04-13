98e51fd742a22b57f66d1ea3a539c3f12fd705a6
SHRUTI <ms.shruti1111@gmail.com>
2025-04-14 01:34:59 +0530

    Push to Git

--- src/ex_10_For_Loop/Lab072_For_Loop_Decrement.java
+++ src/ex_10_For_Loop/Lab072_For_Loop_Decrement.java
@@ -5,15 +5,26 @@ public class Lab072_For_Loop_Decrement {
     public static void main(String[] args) {
 
         /*
-        for (int i = -1; i < -10 ; i--)    // Invalid (Not allowed)
+
+        for (int i = 0; i < 0; i--)    // No output as Condition is not fulfilled.
+        {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i < -10; i--)    // No output as Condition is not fulfilled
+        {
+            System.out.println(i);
+        }
+
+        for (int i = -1; i < -10; i--)    // No output as Condition is not fulfilled
         {
             System.out.println(i);
         }
 
          */
 
-        int i = 10;
-        for ( ; i > 0; i--)         // 10 to 1 (10 times)
+        int i = 10;         // Only Initialisation part can be done outside but Condition and Updation part should always be inside For Loop.
+        for ( ; i > 0; i--)        // 10 to 1 (10 times)
         {
             System.out.println(i);
         }
