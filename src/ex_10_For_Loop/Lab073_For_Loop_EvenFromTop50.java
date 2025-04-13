13fa489cdbb0e95ffc4a96d7d3415c34c721d7e3
SHRUTI <ms.shruti1111@gmail.com>
2025-04-14 02:49:59 +0530

    Push to Git

--- src/ex_10_For_Loop/Lab073_For_Loop_EvenFromTop50.java
+++ src/ex_10_For_Loop/Lab073_For_Loop_EvenFromTop50.java
@@ -5,7 +5,7 @@ public class Lab073_For_Loop_EvenFromTop50 {
     public static void main(String[] args) {
 
         // TO find the even numbers from 1 to 50
-        // even -> i%2 ==0
+        // Even -> i%2 ==0
 
         for (int i = 0; i < 50; i++)
         {
